6ee7e4d2e93c1045a8c0fbaa5e3e820433de3ee4
Walter Mayer <waltermayer@outlook.es>
2023-05-15 18:29:25 -0300

    register finished

--- js/auth/register.js
+++ js/auth/register.js
@@ -8,25 +8,42 @@ registerForm.addEventListener ("submit", (event) => {
 
     event.preventDefault();
 
-    const element = event.target.element;
+    const element = event.target.elements;
 
     if (element.password1.value !== element.password2.value) {
         console.warn(`El password no consigue`);
         return;
     } 
 
-    const users = JSON.parselocalStorage.getItem('users')
+    const users = JSON.parse(localStorage.getItem('users')) || [] ;
+    // si es null, es un array vacio [] para que no nos rompa el codigovich || [] = (o un array vacio)
+
+    const userEmailExist = users.some((usr) => usr.email === element.email.value);
+
+
+    if (userEmailExist) {
+        console.warn(`El correo electrónico ya existe`);
+        return;
+    }
 
 
     const user = {
-        name: elements.fullName.value,
-        email: elements.email.value,
-        password: elements.password1.value,
-        age: elements.age.valueAsNumber,
-        born: elements.born.valueAsNumber,
+        name: element.fullName.value,
+        email: element.email.value,
+        password: element.password1.value,
+        age: element.age.valueAsNumber,
+        born: element.born.valueAsNumber,
+        genre: element.genre.value,
+        country: element.country.value,
 
     }
 
-})
+    users.push(user);
+    localStorage.setItem('users', JSON.stringify(users));
 
+    console.log(`Usuario agregado: ${JSON.stringify(user)}`);
 
+    registerForm.reset();
+});
+
+//!!!!!!! INCORPORAR SWEET ALERT CUANDO EL MAIL YA EXISTE Y CUANDO EL USUSARIO ES CREADOVICH
